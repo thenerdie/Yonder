fb8a5c3231670ba0af482a8940b7d5140a550203
Regen_erate <47013933+thenerdie@users.noreply.github.com>
2020-03-16 15:26:01 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,16 @@
+--[[
+
+CLASS MAIN.LUA
+
+I would highly recommend literally copy/pasting this script VERBATUM into your project. Game logic is not defined here anyway; it is defined in your screens,
+with the exception of global game logic. If you have global game logic you'd like to implement, feel free to put it here.
+
+]]--
+
 local ScreenManager = require("ScreenManager")
 
 function love.load()
-    ScreenManager:SwitchStates("landing", true)
-    ScreenManager:Load()
+    ScreenManager:SwitchStates("landing")
 end
 
 function love.draw()
