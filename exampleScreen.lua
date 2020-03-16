ed617ff1d673a20f1dbbb152be5a6a7c2f456339
Regen_erate <47013933+thenerdie@users.noreply.github.com>
2020-03-16 15:23:01 -0400

    Create exampleScreen.lua

---
+++ exampleScreen.lua
@@ -0,0 +1,36 @@
+--[[
+
+CLASS SAMPLE SCREEN
+
+This is a sample screen. In order to make a new gamestate, or screen, you should copy and paste this code
+into a new Lua module so you can write the logic for a new gamestate. Be sure to add the gamestate to the ScreenManager!
+
+]]--
+
+local screen = {}
+
+function screen:Load(ScreenManager) -- pass a reference to the ScreenManager. Avoids circlular require()
+    
+end
+
+function screen:Draw()
+  
+end
+
+function screen:Update(dt)
+  
+end
+
+function screen:MousePressed(x, y, button)
+  
+end
+
+function screen:MouseReleased(x, y, button)
+  
+end
+
+function screen:KeyPressed(key)
+
+end
+
+return screen
