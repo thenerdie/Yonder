43fcc6c4771e0862165ebc5e78910b303d764f81
Regen_erate <47013933+thenerdie@users.noreply.github.com>
2020-03-17 14:34:10 -0400

    Update ScreenManager.lua

--- ScreenManager.lua
+++ ScreenManager.lua
@@ -12,23 +12,40 @@ For details check out main.lua in this repo: https://github.com/thenerdie/Yonder
 
 ]]--
 
-local gameStates = { -- this is where you set the directories of your screens
-	["exampleScreen"] = require("exampleScreen")
-}
+local verbose = true
+local very_verbose = false
 
--- dont touch anything below unless you know what you are doing :)
+local function puts(str, reqVeryVerbosePerms)
+	if reqVeryVerbosePerms then
+		if verbose and very_verbose then
+			print(str)
+		end
+	else
+		if verbose then
+			print(str)
+		end
+	end
+end
+
+local gameStates = { -- this is where you set the directories of your screens
+	["landing"] = require("Screens/LandingScreen");
+	["songs"] = require("Screens/SongSelect")
+}
 
 local currState = nil
 
 local self = {}
 -----------------------
-function self:SwitchStates(newState, suppressLoad)
+function self:SwitchStates(newState, supressLoad)
 	if not gameStates[newState] then
 		currState = nil
 		error("Error SM106: newState not found in gameStates")
 	else
 		currState = newState
-		if not suppressLoad then self:Load() end
+		if not supressLoad then
+			puts("Loaded screen \"" .. newState .. "\"; :Load() suppressed = " .. tostring(not not supressLoad))
+			self:Load()
+		end
 	end
 end
 --// Common LOVE2D functions, wrapped in ScreenManager
@@ -43,6 +60,9 @@ function self:Update(dt)
 	if currState ~= nil then
 		if gameStates[currState].Update then
 			gameStates[currState]:Update(dt)
+			puts(":Update called; DT = " .. dt .. "; FPS = " .. 1/dt, true)
+		else
+			puts(":Update called but unimplemented; DT = " .. dt .. "; FPS = " .. 1/dt, true)
 		end
 	end
 end
@@ -50,6 +70,7 @@ function self:Quit()
 	if currState ~= nil then
 		if gameStates[currState].Quit then
 			gameStates[currState]:Quit()
+			puts("Exiting game...")
 		end
 	end
 end
@@ -57,6 +78,7 @@ function self:MousePressed(x, y, button)
 	if currState ~= nil then
 		if gameStates[currState].MousePressed then
 			gameStates[currState]:MousePressed(x, y, button)
+			puts("Mouse pressed; x = " .. x .. "; y = " .. y)
 		end
 	end
 end
@@ -64,6 +86,7 @@ function self:MouseReleased(x, y, button)
 	if currState ~= nil then
 		if gameStates[currState].MouseReleased then
 			gameStates[currState]:MouseReleased(x, y, button)
+			puts("Mouse released; x = " .. x .. "; y = " .. y)
 		end
 	end
 end
@@ -71,6 +94,7 @@ function self:KeyPressed(key)
 	if currState ~= nil then
 		if gameStates[currState].KeyPressed then
 			gameStates[currState]:KeyPressed(key)
+			puts("Key pressed; key = " .. key)
 		end
 	end
 end
@@ -78,6 +102,7 @@ function self:KeyReleased(key)
 	if currState ~= nil then
 		if gameStates[currState].KeyReleased then
 			gameStates[currState]:KeyReleased(key)
+			puts("Key released; key = " .. key)
 		end
 	end
 end
@@ -85,8 +110,20 @@ function self:Load()
 	if currState ~= nil then
 		if gameStates[currState].Load then
 			gameStates[currState]:Load(self)
+			puts("Loaded screen \"" .. currState .. "\" successfully!")
+		else
+			puts(":Load() unimplemented in Screen")
 		end
 	end
 end
+function self:MouseMoved(x, y, dx, dy, istouch)
+	if currState ~= nil then
+		if gameStates[currState].MouseMoved then
+			gameStates[currState]:MouseMoved(x, y, dx, dy, istouch)
+			puts("Mouse moved; x = " .. x .. "; y = " .. y, true)
+		end
+	end
+end
+puts("ScreenManager class initialized successfully!")
 -----------------------
 return self
